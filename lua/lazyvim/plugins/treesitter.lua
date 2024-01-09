4f7ee3dd3cb57df0f90496d0a91c39028e7d5712
Data8021 <bwales@gmail.com>
2024-01-09 10:47:54 -0500

    tryign to fix treesitter

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -16,7 +16,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dir = require("lazy-nix-helper").get_plugin_path("nvim-treesitter"),
     version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
+    --build = ":TSUpdate",
     event = { "LazyFile", "VeryLazy" },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     init = function(plugin)
