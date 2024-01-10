9c45e39eaa4abb2ba4d7fc4c4bcd42c5eaab9543
Data8021 <bwales@gmail.com>
2024-01-09 21:43:23 -0500

    undo nil lsp

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -33,7 +33,6 @@ return {
             },
           },
         },
-<<<<<<< HEAD
         -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the inlay hints.
