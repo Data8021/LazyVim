c8e99b8c5a19dfaa8b21b0e5d2ca002d52015209
Data8021 <bwales@gmail.com>
2024-01-09 21:43:23 -0500

    undo nil lsp

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -62,9 +62,6 @@ return {
             },
           },
         },
-        nil = {
-          mason = false,
-        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
