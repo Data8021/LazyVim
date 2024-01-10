af547596605b932babf981c3870c463316ad3541
Data8021 <bwales@gmail.com>
2024-01-09 21:39:01 -0500

    misssing , in lsp init

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -64,7 +64,7 @@ return {
         },
         nil = {
           mason = false,
-        }
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
